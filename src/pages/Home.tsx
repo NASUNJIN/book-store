ae4a8a5ddd9e69af28731125f8cecd7da3103184
NASUNJIN <sj010618@gmail.com>
2024-07-19 19:29:33 +0900

    feat: InputText 컴포넌트 작성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/common/Button";
+import InputText from "../components/common/InputText";
 import Title from "../components/common/Title";
 
 function Home() {
@@ -6,6 +7,7 @@ function Home() {
         <>
             <Title size="medium" color="background">제목 테스트</Title>
             <Button size="large" scheme="primary">버튼 테스트</Button>
+            <InputText placeholder="여기에 입력하세요" />
             <div>home body</div>
         </>
     );
