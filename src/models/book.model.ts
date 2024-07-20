6e2b61cec7a1d178b099d49f03be373bbce134f7
NASUNJIN <sj010618@gmail.com>
2024-07-20 21:29:21 +0900

    chore: model 추가

---
+++ src/models/book.model.ts
@@ -0,0 +1,19 @@
+export interface Book {
+    id: number;
+    title: string;
+    img: number;
+    category_id: number;
+    form: string;
+    isbn: string;
+    summary: string;
+    detail: string;
+    author: string;
+    pages: number;
+    likes: number;
+    pubDate: string;
+};
+
+export interface BookDetail extends Book {
+    categoryName: string;
+    liked: boolean;
+}
