1adb59b33f441297a774e7435a831fd61d8f4c4a
NASUNJIN <sj010618@gmail.com>
2024-07-20 21:12:54 +0900

    chore: 오타 수정

--- src/components/common/InputText.tsx
+++ src/components/common/InputText.tsx
@@ -16,7 +16,7 @@ const InputTextstyled = styled.input.attrs({ type: "text"})`
     padding: 0.25rem, 0.75rem;
     border: 1px solid ${({ theme }) => theme.color.border};
     border-radius: ${({ theme }) => theme.borderRadius.default};
-    font-szie : 1rem;
+    font-size : 1rem;
     line-height : 1.5;
     color: ${({ theme }) => theme.color.text};
 `;
