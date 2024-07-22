f45b13be7c3788dfb9be8a9178358a6b172bbd25
NASUNJIN <sj010618@gmail.com>
2024-07-23 01:18:21 +0900

    feat: 이전에 만든 book-shop과 api통신

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,27 +2,11 @@ import { styled } from "styled-components";
 import logo from "../../assets/images/logo.png";
 import { FaSignInAlt, FaRegUser } from "react-icons/fa"
 import { Link } from "react-router-dom";
-
-const CATEGORY = [
-    {
-        id: null,
-        name: "전체",
-    },
-    {
-        id: 0,
-        name: "동화",
-    },
-    {
-        id: 1,
-        name: "소설",
-    },
-    {
-        id: 2,
-        name: "사회",
-    },
-];
+import { useCategory } from "../../hooks/useCategory";
 
 function Header() {
+    const { category } = useCategory();
+
     return (
         <HeaderStyle>
             <h1 className="logo">
@@ -33,10 +17,10 @@ function Header() {
             <nav className="category">
                 <ul>
                     {
-                        CATEGORY.map((item) => (
-                            <li key={item.id}>
-                                <Link to={item.id ===null ? "books" : `/books?category_id=${item.id}`}>
-                                    {item.name}
+                        category.map((item) => (
+                            <li key={item.category_id}>
+                                <Link to={item.category_id ===null ? "books" : `/books?category_id=${item.category_id}`}>
+                                    {item.category_name}
                                 </Link>
                             </li>
                         ))
