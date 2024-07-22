9ce72c80ca2cd234aac93712c912905d930f8d1b
NASUNJIN <sj010618@gmail.com>
2024-07-23 02:55:00 +0900

    feat: 비밀번호 초기화(토큰 미사용)

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,7 +1,20 @@
 import { SignupProps } from "../pages/Signup";
 import { httpClient } from "./http";
 
+// 회원가입
 export const signup = async(userData: SignupProps) => {
     const response = await httpClient.post("/users/join", userData);
+    return response.data;
+};
+
+// 비밀번호 초기화 요청
+export const resetRequest = async(data: SignupProps) => {
+    const response = await httpClient.post("/users/reset", data);
+    return response.data;
+};
+
+// 비밀번호 초기화
+export const resetPassword = async(data: SignupProps) => {
+    const response = await httpClient.put("/users/reset", data);
     return response.data;
 };
