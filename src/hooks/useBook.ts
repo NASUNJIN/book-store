a7b57d7b0d2bef22b34aa5762d43252d2b9ac9d8
NASUNJIN <sj010618@gmail.com>
2024-08-02 02:38:40 +0900

    fix: useAlert.ts showConfirm 추가로 인한 코드수정

--- src/hooks/useBook.ts
+++ src/hooks/useBook.ts
@@ -10,7 +10,7 @@ export const useBook = (bookId: string | undefined) => {
     // 장바구니로 이동 alert
     const [cartAdded, setCartAdded] = useState(false);
     const { isloggedIn } = useAuthStore();
-    const showAlert = useAlert();
+    const { showAlert } = useAlert();
 
     const likeToggle = () => {
         // 권한 확인
